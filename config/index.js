90bb645a2c443b220f543b451ecf52119746f9cd
Manchio <1275566519@qq.com>
2021-05-19 13:12:01 +0800

    '初始化项目'

--- config/index.js
+++ config/index.js
@@ -13,7 +13,8 @@ module.exports = {
     proxyTable: {},
 
     // Various Dev Server settings
-    host: 'localhost', // can be overwritten by process.env.HOST
+    //host: 'localhost', // can be overwritten by process.env.HOST
+    host: "172.26.114.132",
     port: 8080, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     useLocalIp: true,
     autoOpenBrowser: false,
